cd693ea2316187d04be69ee1c86aa6e026b22f1b
baamudeizy <g22933om@gm.tsuda.ac.jp>
2024-05-10 16:06:03 +0900

    ごきげんよう

--- src/Hellosan.java
+++ src/Hellosan.java
@@ -4,6 +4,7 @@ public class Hellosan {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello world");
+		System.out.println("Good Afternoon World");
 
 	}
 
